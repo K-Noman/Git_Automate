abb88f67555e03b7a21d25714146af0deb2e0b8a
Noman <chnoman503@gmail.com>
2020-07-10 22:20:47 +0500

    Path modification

--- BrowseActivity.py
+++ BrowseActivity.py
@@ -1,10 +1,14 @@
 import sys
 import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 path = r"C:\Users\9 MAN\projects\automate project\chromedriver.exe"
+dirpath = os.getcwd()
+path = f' {dirpath}\chromedriver.exe'
+print(path)
 
-repository = "noman "
+repository = str(sys.argv[1])
 print(repository)
-os.chdir(repository)
-os.pardir
-print("All set to go ")
--- br2.py
+++ br2.py
@@ -6,6 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 path = r"C:\Users\9 MAN\projects\automate project\chromedriver.exe"
+dirpath = os.getcwd()
+path = f' {dirpath}\chromedriver.exe'
+print(path)
 
 repository = str(sys.argv[1])
 print(repository)
